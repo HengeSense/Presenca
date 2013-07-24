c68c7aeb3939fbf6d2f163163d636c5d90355fc2
Pedro Goes <pedro.pecanha.goes@gmail.com>
2013-07-23 23:08:24 -0300

    Members

--- Presenca-release/Web/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("includes/loginCheck.php");
-
-	$filename = basename($_SERVER['PHP_SELF']);
-	$path = str_replace($filename, '', $_SERVER['PHP_SELF']);
-
-	setcookie($chave, '', 0, $path);
-	header("Location: $path");	
-?>
